4c29e6e6acb720cf28796acb7a0371fe9591483d
Aschen <adrien@maret.email>
2021-01-06 17:40:32 +0100

    wip

---
+++ lib/controllers/EngineController.ts
@@ -0,0 +1,105 @@
+import {
+  ControllerDefinition,
+  KuzzleRequest,
+  PreconditionError,
+  PluginContext,
+  KuzzleRequest,
+  EmbeddedSDK,
+} from 'kuzzle';
+
+import { NativeController } from 'kuzzle/lib/api/controller/base.js'
+
+export class EngineController extends NativeController {
+  [key: string]: any;
+
+  get sdk (): EmbeddedSDK {
+    return this.context.accessors.sdk;
+  }
+
+  private context: PluginContext;
+  public definition: ControllerDefinition;
+
+  /**
+   * Constructor
+   *
+   * @param context
+   */
+  constructor (context) {
+    super(context['kuzzle']);
+
+    this.context = context;
+
+    this.definition = {
+      actions: {
+        create: {
+          handler: this.create.bind(this),
+          http: [{ verb: 'post', path: 'device-manager/engine/:index' }],
+        },
+        delete: {
+          handler: this.delete.bind(this),
+          http: [{ verb: 'delete', path: 'device-manager/engine/:index' }],
+        },
+        list: {
+          handler: this.list.bind(this),
+          http: [{ verb: 'get', path: 'device-manager/engines' }],
+        },
+      },
+    };
+  }
+
+  async create (request: KuzzleRequest) {
+    const index = this.getIndex(request);
+    const refresh = this.getRefresh(request);
+
+    const collections = this.context.config.collections;
+
+    const promises = [];
+
+    for (const [collection, mappings] of Object.entries(collections)) {
+      promises.push(this.sdk.collection.create(index, collection, { mappings }));
+    }
+
+    await Promise.all(promises);
+
+    await this.sdk.document.create(
+      this.config.adminIndex,
+      'engines',
+      { index },
+      index,
+      { refresh: 'wait_for' })
+
+    return { index, collections };
+  }
+
+  async delete (request: KuzzleRequest) {
+    const index = this.getIndex(request);
+
+    const collections = Object.keys(this.context.config.collections);
+
+    const promises = [];
+
+    for (const collection of collections) {
+      promises.push(this.sdk.collection.delete(index, collection));
+    }
+
+    await Promise.all(promises);
+
+    await this.sdk.document.delete(
+      this.config.adminIndex,
+      'engines',
+      index,
+      { refresh: 'wait_for' });
+
+    return { index, collections };
+  }
+
+  async list () {
+    const result = await this.sdk.document.search(
+      this.config.adminIndex,
+      'engines',
+      {},
+      { size: 1000 });
+
+    return result.hits.map(hit => hit._source as any);
+  }
+}
