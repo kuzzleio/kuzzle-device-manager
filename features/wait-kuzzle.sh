ed98de2ede2651ca1a858a4ffbdf68dab985aaee
Aschen <adrien@maret.email>
2021-01-20 10:28:25 +0100

    Run tests with docker compose and node vanilla

--- features/wait-kuzzle.sh
+++ features/wait-kuzzle.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-node app.js &
-
-echo "[$(date)] - Starting Kuzzle..."
+echo "[$(date)] - Waiting Kuzzle..."
 while ! curl -f -s -o /dev/null http://localhost:7512
 do
     echo "[$(date)] - Still trying to connect to Kuzzle"
