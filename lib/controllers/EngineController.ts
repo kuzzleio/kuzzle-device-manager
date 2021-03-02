8f546e272cdca0045904f9f706951a62ac8a3144
Aschen <adrien@maret.email>
2021-03-02 08:08:21 +0100

    Adapte for Kuzzle 2.10

--- lib/controllers/EngineController.ts
+++ lib/controllers/EngineController.ts
@@ -7,7 +7,7 @@ import {
   JSONObject,
 } from 'kuzzle';
 
-import { NativeController } from 'kuzzle/lib/api/controller/base.js'
+import { NativeController } from 'kuzzle/lib/api/controllers/baseController.js'
 
 export class EngineController extends NativeController {
   [key: string]: any;
