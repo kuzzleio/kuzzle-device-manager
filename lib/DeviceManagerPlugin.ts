16a864f4197668ab74fd0483e2ab127d89111df8
Aschen <adrien@maret.email>
2021-03-23 14:05:21 +0100

    Remove multitenancy pipes

--- lib/DeviceManagerPlugin.ts
+++ lib/DeviceManagerPlugin.ts
@@ -94,28 +94,6 @@ export class DeviceManagerPlugin extends Plugin {
       }
     };
 
-    this.pipes = {
-      'multi-tenancy/tenant:afterCreate': async request => {
-        const { index: tenantIndex } = request.result;
-
-        const { collections } = await this.engineService.create(tenantIndex);
-
-        if (!Array.isArray(request.result.collections)) {
-          request.result.collections = [];
-        }
-        request.result.collections.push(...collections);
-
-        return request;
-      },
-      'multi-tenancy/tenant:afterDelete': async request => {
-        const { index: tenantIndex } = request.result;
-
-        await this.engineService.delete(tenantIndex);
-
-        return request;
-      }
-    };
-
     this.defaultConfig = {
       adminIndex: 'device-manager',
       adminCollections: {
