731b67384b96b5d52bd85cecde0761162f543aff
Aschen <adrien@maret.email>
2021-04-07 10:41:08 +0200

    change ID separator

--- features/fixtures/fixtures.js
+++ features/fixtures/fixtures.js
@@ -4,7 +4,7 @@ module.exports = {
   'device-manager': {
     devices: [
       ...devices,
-      { index: { _id: 'DummyTemp_detached' } },
+      { index: { _id: 'DummyTemp-detached' } },
       {
         reference: 'detached',
         model: 'DummyTemp',
@@ -13,9 +13,9 @@ module.exports = {
         tenantId: null,
         assetId: null
       },
-      { index: { _id: 'DummyTemp_attached-ayse-unlinked' } },
+      { index: { _id: 'DummyTemp-attached_ayse_unlinked' } },
       {
-        reference: 'attached-ayse-unlinked',
+        reference: 'attached_ayse_unlinked',
         model: 'DummyTemp',
         measures: {
           temperature: {
@@ -34,9 +34,9 @@ module.exports = {
   },
   'tenant-ayse': {
     devices: [
-      { index: { _id: 'DummyTemp_attached-ayse-unlinked' } },
+      { index: { _id: 'DummyTemp-attached_ayse_unlinked' } },
       {
-        reference: 'attached-ayse-unlinked',
+        reference: 'attached_ayse_unlinked',
         model: 'DummyTemp',
         measures: {
           temperature: {
