0a64847777ff2d2c8b3be89041a68a9d1dad453d
jenow <jenova.ke20@gmail.com>
2021-04-09 10:01:20 +0200

    fix post review

--- lib/controllers/CRUDController.ts
+++ lib/controllers/CRUDController.ts
@@ -32,7 +32,7 @@ export class CRUDController extends NativeController {
    *
    * @param request
    */
-  create (request: KuzzleRequest) {
+  async create (request: KuzzleRequest) {
     const index = this.getIndex(request);
     const asset = this.getBody(request);
     const id = request.input.resource._id;
@@ -50,7 +50,7 @@ export class CRUDController extends NativeController {
    *
    * @param request
    */
-  delete (request: KuzzleRequest) {
+  async delete (request: KuzzleRequest) {
     const index = this.getIndex(request);
     const id = this.getId(request);
 
@@ -66,7 +66,7 @@ export class CRUDController extends NativeController {
    *
    * @param request
    */
-  search (request: KuzzleRequest) {
+  async search (request: KuzzleRequest) {
     const index = this.getIndex(request);
     const { searchBody } = this.getSearchParams(request);
 
@@ -77,8 +77,7 @@ export class CRUDController extends NativeController {
         index,
         collection: this.collection,
         body: searchBody,
-        from: searchBody.from,
-        size: searchBody.size
+        ...request.input.args
       }
     );
   }
@@ -88,7 +87,7 @@ export class CRUDController extends NativeController {
    *
    * @param request
    */
-  update (request: KuzzleRequest) {
+  async update (request: KuzzleRequest) {
     const index = this.getIndex(request);
     const body = this.getBody(request);
     const id = this.getId(request);
