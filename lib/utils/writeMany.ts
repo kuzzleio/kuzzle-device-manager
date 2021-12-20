6a29a2125ff1fe7952e24b0350b1e7a2d9fc51e4
Nicolas <nriquelmebareiro@kuzzle.io>
2021-12-20 09:38:20 +0100

    Feat/asset import csv (#140)
    * wip
    
    * end of feature?
    
    * nah, missing doc but now?
    
    * typo
    
    * requested changes
    
    * some minor fixes and remove logs
    
    * fix ci
    
    * use concat instead of ...array
    
    * add return type to writeToDatabase function
    
    * Update doc/1/controllers/asset/import-assets/index.md
    
    Co-authored-by: Adrien Maret <amaret93@gmail.com>
    
    * Update doc/1/controllers/asset/import-assets/index.md
    
    Co-authored-by: Adrien Maret <amaret93@gmail.com>
    
    * Update doc/1/controllers/asset/import-assets/index.md
    
    Co-authored-by: Adrien Maret <amaret93@gmail.com>
    
    * requested changes
    
    Co-authored-by: Adrien Maret <amaret93@gmail.com>

---
+++ lib/utils/writeMany.ts
@@ -0,0 +1,57 @@
+import { JSONObject } from "kuzzle";
+
+export type mRequest = {
+  _id: string;
+  body: JSONObject;
+}
+
+export type mResponse = {
+  successes: JSONObject[];
+  errors: JSONObject[];
+}
+
+export async function writeToDatabase (
+  documents: mRequest[],
+  writer: (documents: mRequest[]) => Promise<mResponse>
+) {
+  const results = {
+    errors: [],
+    successes: [],
+  }
+
+  const limit = global.kuzzle.config.limits.documentsWriteCount;
+
+  if (documents.length <= limit) {
+    const { successes, errors } = await writer(documents);
+    results.successes.concat(successes);
+    results.errors.concat(errors);
+
+    return results;
+  }
+
+  const writeMany = async (start: number, end: number) => {
+    const devices = documents.slice(start, end);
+    const { successes, errors } = await writer(devices);
+
+    results.successes.concat(successes);
+    results.errors.concat(errors);
+  };
+
+  let offset = 0;
+  let offsetLimit = limit;
+  let done = false;
+
+  while (! done) {
+    await writeMany(offset, offsetLimit)
+
+    offset += limit;
+    offsetLimit += limit;
+
+    if (offsetLimit >= documents.length) {
+      done = true;
+      await writeMany(offset, documents.length);
+    }
+  }
+
+  return results;
+}
