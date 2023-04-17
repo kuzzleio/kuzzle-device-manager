ba2ff1b83c9fb641b39fc6d51470096050457b39
Olivier Cavadenti <olivier.cavadenti@gmail.com>
2023-04-17 09:27:42 +0200

    rename + const

--- lib/modules/device/DeviceService.ts
+++ lib/modules/device/DeviceService.ts
@@ -461,15 +461,15 @@ export class DeviceService {
 
       const updatedMeasureNames: MeasureName[] = [];
       for (const measure of measureNames) {
-        let findedMeasure = deviceModel.device.measures.find(
+        const foundMeasure = deviceModel.device.measures.find(
           (deviceMeasure) => deviceMeasure.name === measure.device
         );
-        if (!findedMeasure && !implicitMeasuresLinking) {
+        if (!foundMeasure && !implicitMeasuresLinking) {
           throw new BadRequestError(
             `Measure "${measure.asset}" is not declared in the device model "${measure.device}".`
           );
         }
-        const { type } = findedMeasure;
+        const { type } = foundMeasure;
         updatedMeasureNames.push({
           asset: measure.asset,
           device: measure.device,
