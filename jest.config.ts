442902c2ffcf2ecb2f00c55504045ab76424f104
Sébastien Rodriguez <sebtiz13@gmail.com>
2023-10-31 12:41:21 +0100

    test: fix erratic bug in functional tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
+  testTimeout: 20000,
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tests/tsconfig.json" }],
   },
