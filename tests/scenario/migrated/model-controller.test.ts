5e14baa9e1949cd772a2d71778d047ff98fd5f3d
afondard <afondard@kuzzle.io>
2024-05-15 16:12:25 +0200

    fix tests

--- tests/scenario/migrated/model-controller.test.ts
+++ tests/scenario/migrated/model-controller.test.ts
@@ -588,7 +588,6 @@ describe("features/Model/Controller", () => {
     expect(response._source).toMatchObject({
       type: "asset",
       engineGroup: "commons",
-      model: "AdvancedWarehouse",
       asset: assetModelWithTooltip,
     });
 });
