0bd9d3c65032e9d92a7a12001292769eac8ea3ff
Allan Merland <amerland@kuzzle.io>
2024-05-23 09:37:03 +0200

    feat(measure): measure sources types

---
+++ lib/modules/measure/types/MeasureSources.ts
@@ -0,0 +1,30 @@
+import { Metadata } from "../../shared";
+
+interface AbstractMeasureSource {
+  type: string;
+  dataSourceId: string;
+  indexId: string;
+  assetId: string;
+  customMetadata: Metadata;
+  lastMeasuredAt?: number;
+}
+
+export interface DeviceMeasureSource extends AbstractMeasureSource {
+  type: "device";
+  reference?: string;
+  model: string;
+}
+
+export interface APIMeasureSource extends AbstractMeasureSource {
+  type: "api";
+}
+
+export function isSourceDevice(source: any): source is DeviceMeasureSource {
+  return source?.type === "device";
+}
+
+export function isSourceAPI(source: any): source is APIMeasureSource {
+  return source?.type === "api";
+}
+
+export type MeasureSource = DeviceMeasureSource | APIMeasureSource;
