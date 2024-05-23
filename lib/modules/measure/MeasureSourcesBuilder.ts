a9045e28d3eb15f59bce8c5a00835b928cf86f23
Allan Merland <amerland@kuzzle.io>
2024-05-23 09:37:03 +0200

    feat(measure): measure sources converter

---
+++ lib/modules/measure/MeasureSourcesBuilder.ts
@@ -0,0 +1,70 @@
+import { Metadata } from "../shared";
+import { MeasureOriginApi, MeasureOriginDevice } from "./types/MeasureContent";
+import { APIMeasureSource, DeviceMeasureSource } from "./types/MeasureSources";
+
+export function toApiSource(
+  dataSourceId: string,
+  assetId: string,
+  indexId: string,
+  customMetadata: Metadata = {},
+  lastMeasuredAt?: number,
+): APIMeasureSource {
+  return {
+    assetId,
+    customMetadata,
+    dataSourceId,
+    indexId,
+    lastMeasuredAt,
+    type: "api",
+  };
+}
+
+export function apiSourceToOriginApi(
+  source: APIMeasureSource,
+  measureName: string,
+  payloadUuids: string[],
+): MeasureOriginApi {
+  return {
+    _id: source.dataSourceId,
+    measureName,
+    payloadUuids,
+    type: "api",
+  };
+}
+
+export function toDeviceSource(
+  dataSourceId: string,
+  reference: string,
+  model: string,
+  assetId: string,
+  indexId: string,
+  customMetadata: Metadata,
+  lastMeasuredAt?: number,
+): DeviceMeasureSource {
+  return {
+    assetId,
+    customMetadata,
+    dataSourceId,
+    indexId,
+    lastMeasuredAt,
+    model,
+    reference,
+    type: "device",
+  };
+}
+
+export function deviceSourceToOriginDevice(
+  source: DeviceMeasureSource,
+  measureName: string,
+  payloadUuids: string[],
+): MeasureOriginDevice {
+  const { dataSourceId, model, reference } = source;
+  return {
+    _id: dataSourceId,
+    deviceModel: model,
+    measureName,
+    payloadUuids,
+    reference: reference,
+    type: "device",
+  };
+}
