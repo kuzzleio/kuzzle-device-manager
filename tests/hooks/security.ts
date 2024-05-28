39190fc97f2d183addea5208fb41ad6b581c8add
Quentin <qrousselet@kuzzle.io>
2024-05-28 09:51:50 +0200

    fix(test): remove admin connexion before all

--- tests/hooks/security.ts
+++ tests/hooks/security.ts
@@ -4,13 +4,6 @@ import rights from "../fixtures/rights";
 
 export async function loadSecurityDefault(sdk: Kuzzle) {
   sdk.jwt = null;
-  // TODO check how to load the fixtures in the CI to pass the test
-  if (!(await sdk.auth.getCurrentUser())._id.includes("admin")) {
-    await sdk.auth.login("local", {
-      username: "admin",
-      password: "password",
-    });
-  }
 
   await sdk.query({
     controller: "admin",
