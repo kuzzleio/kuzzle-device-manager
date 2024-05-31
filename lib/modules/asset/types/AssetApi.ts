6774cc7b3abfe12ccab7be37bc70cf7eed278780
Allan Merland <amerland@kuzzle.io>
2024-05-31 10:52:16 +0200

    fix(asset): query api type

--- lib/modules/asset/types/AssetApi.ts
+++ lib/modules/asset/types/AssetApi.ts
@@ -121,8 +121,9 @@ export type ApiAssetGetMeasuresResult = {
 export interface ApiAssetIngestMeasuresRequest extends AssetsControllerRequest {
   action: "ingestMeasures";
 
+  _id: string;
+
   engineId: string;
-  assetId: string;
   engineGroup?: string;
 
   body: {
