6c43d5760c088d5bd87e8abb69985bf450cfc1be
Alexandre Sollier <github@kuruyia.net>
2024-11-13 17:24:08 +0100

    feat: stop using measures from assets and devices in exporter (#369)

--- tests/scenario/modules/assets/action-export.test.ts
+++ tests/scenario/modules/assets/action-export.test.ts
@@ -10,7 +10,7 @@ import fixtures from "../../../fixtures/fixtures";
 const assetCount = fixtures["engine-ayse"].assets.length / 2;
 jest.setTimeout(10000);
 
-function getExportedColums(row) {
+function getExportedColums(row: string) {
   const parsedRow = csvParse(row)[0];
 
   return {
@@ -20,16 +20,18 @@ function getExportedColums(row) {
     co2: parsedRow[3],
     humidity: parsedRow[4],
     illuminance: parsedRow[5],
-    position: parsedRow[6],
-    positionAccuracy: parsedRow[7],
-    positionAltitude: parsedRow[8],
-    powerConsumptionWatt: parsedRow[9],
-    temperature: parsedRow[10],
-    temperatureExt: parsedRow[11],
-    temperatureInt: parsedRow[12],
-    temperatureWeather: parsedRow[13],
-    lastMeasuredAt: parsedRow[14],
-    lastMeasuredAtISO: parsedRow[15],
+    magiculeExt: parsedRow[6],
+    magiculeInt: parsedRow[7],
+    position: parsedRow[8],
+    positionAccuracy: parsedRow[9],
+    positionAltitude: parsedRow[10],
+    powerConsumptionWatt: parsedRow[11],
+    temperature: parsedRow[12],
+    temperatureExt: parsedRow[13],
+    temperatureInt: parsedRow[14],
+    temperatureWeather: parsedRow[15],
+    lastMeasuredAt: parsedRow[16],
+    lastMeasuredAtISO: parsedRow[17],
   };
 }
 
@@ -37,6 +39,8 @@ describe("AssetsController:exportMeasures", () => {
   const sdk = setupHooks();
 
   it("should prepare export of different assets types and return a CSV as stream", async () => {
+    const measureDate = Date.now();
+
     await sendDummyTempPositionPayloads(sdk, [
       {
         deviceEUI: "warehouse",
@@ -44,7 +48,7 @@ describe("AssetsController:exportMeasures", () => {
         location: { lat: 42.2, lon: 2.42, accuracy: 2100 },
         battery: 0.8,
         // ? Use date now - 1s to ensure this asset are second in export
-        measuredAt: Date.now() - 2000,
+        measuredAt: measureDate - 2000,
       },
       {
         deviceEUI: "linked2",
@@ -52,7 +56,7 @@ describe("AssetsController:exportMeasures", () => {
         location: { lat: 42.2, lon: 2.42, accuracy: 2100 },
         battery: 0.8,
         // ? Use date now to ensure this asset is first in export
-        measuredAt: Date.now(),
+        measuredAt: measureDate,
       },
     ]);
     await sdk.collection.refresh("engine-ayse", "assets");
@@ -64,9 +68,6 @@ describe("AssetsController:exportMeasures", () => {
       controller: "device-manager/assets",
       action: "export",
       engineId: "engine-ayse",
-      body: {
-        sort: { lastMeasuredAt: "desc" },
-      },
     });
 
     expect(typeof result.link).toBe("string");
@@ -85,48 +86,58 @@ describe("AssetsController:exportMeasures", () => {
 
     writeFileSync("./assets.csv", csv.join(""));
 
-    expect(csv[0]).toBe(
+    expect(csv).toHaveLength(assetCount + 1);
+
+    const header = csv.shift();
+
+    expect(header).toBe(
       "Model,Reference,brightness.lumens,co2,humidity,illuminance,magiculeExt,magiculeInt,position,position.accuracy,position.altitude,powerConsumption.watt,temperature,temperatureExt,temperatureInt,temperatureWeather,lastMeasuredAt,lastMeasuredAtISO\n",
     );
 
-    expect(csv).toHaveLength(assetCount + 1);
+    const rows = csv
+      .map(getExportedColums)
+      .sort((a, b) => b.lastMeasuredAt - a.lastMeasuredAt);
 
-    const row1 = getExportedColums(csv[1]);
+    const row1 = rows[0];
 
     expect(row1.model).toBe("Container");
-    expect(typeof row1.reference).toBe("string");
+    expect(row1.reference).toBe("linked2");
+    expect(row1.position).toBe('{"lat":42.2,"lon":2.42}');
+    expect(parseFloat(row1.positionAccuracy)).toBe(2100);
+    expect(parseFloat(row1.temperatureExt)).toBe(23.3);
+    expect(parseFloat(row1.lastMeasuredAt)).toBe(measureDate);
+    expect(row1.lastMeasuredAtISO).toBe(new Date(measureDate).toISOString());
+
     expect(typeof parseFloat(row1.brightnessLumens)).toBe("number");
     expect(typeof parseFloat(row1.co2)).toBe("number");
     expect(typeof parseFloat(row1.humidity)).toBe("number");
     expect(typeof parseFloat(row1.illuminance)).toBe("number");
-    expect(typeof row1.position).toBe("string");
-    expect(typeof parseFloat(row1.positionAccuracy)).toBe("number");
     expect(typeof parseFloat(row1.positionAltitude)).toBe("number");
     expect(typeof parseFloat(row1.powerConsumptionWatt)).toBe("number");
     expect(typeof parseFloat(row1.temperature)).toBe("number");
-    expect(typeof parseFloat(row1.temperatureExt)).toBe("number");
     expect(typeof parseFloat(row1.temperatureInt)).toBe("number");
     expect(typeof parseFloat(row1.temperatureWeather)).toBe("number");
-    expect(typeof parseFloat(row1.lastMeasuredAt)).toBe("number");
-    expect(typeof row1.lastMeasuredAtISO).toBe("string");
 
-    const row2 = getExportedColums(csv[2]);
+    const row2 = rows[1];
 
     expect(row2.model).toBe("Warehouse");
-    expect(typeof row2.reference).toBe("string");
+    expect(row2.reference).toBe("linked");
+    expect(row2.position).toBe('{"lat":42.2,"lon":2.42}');
+    expect(parseFloat(row2.positionAccuracy)).toBe(2100);
+    expect(parseFloat(row2.lastMeasuredAt)).toBe(measureDate - 2000);
+    expect(row2.lastMeasuredAtISO).toBe(
+      new Date(measureDate - 2000).toISOString(),
+    );
+
     expect(typeof parseFloat(row2.brightnessLumens)).toBe("number");
     expect(typeof parseFloat(row2.co2)).toBe("number");
     expect(typeof parseFloat(row2.humidity)).toBe("number");
     expect(typeof parseFloat(row2.illuminance)).toBe("number");
-    expect(typeof row2.position).toBe("string");
-    expect(typeof parseFloat(row2.positionAccuracy)).toBe("number");
     expect(typeof parseFloat(row2.positionAltitude)).toBe("number");
     expect(typeof parseFloat(row2.powerConsumptionWatt)).toBe("number");
     expect(typeof parseFloat(row2.temperature)).toBe("number");
     expect(typeof parseFloat(row2.temperatureExt)).toBe("number");
     expect(typeof parseFloat(row2.temperatureInt)).toBe("number");
     expect(typeof parseFloat(row2.temperatureWeather)).toBe("number");
-    expect(typeof parseFloat(row2.lastMeasuredAt)).toBe("number");
-    expect(typeof row2.lastMeasuredAtISO).toBe("string");
   });
 });
