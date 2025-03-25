69ed8439723fd13ba115fcf13b70d678b9029607
Cyril Nguyen <cnguyen@kuzzle.io>
2025-03-25 15:29:37 +0100

    feat: export payload type

--- lib/modules/decoder/exports.ts
+++ lib/modules/decoder/exports.ts
@@ -1,5 +1,7 @@
 export * from "./types/DecoderApi";
 export * from "./types/DecoderContent";
+export * from "./types/PayloadApi";
+export * from "./types/PayloadContent";
 export * from "./DecodedPayload";
 export * from "./Decoder";
 export * from "./roles/RolePayloadsAll";
