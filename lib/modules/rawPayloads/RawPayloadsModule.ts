8a586e771b7cb7751870f555ab234e64db587362
Cyril Nguyen <cnguyen@kuzzle.io>
2025-03-26 16:16:43 +0100

    refactor: remove right not necessary right and add export

--- lib/modules/rawPayloads/RawPayloadsModule.ts
+++ lib/modules/rawPayloads/RawPayloadsModule.ts
@@ -2,7 +2,6 @@ import { Module } from "../shared/Module";
 
 import { RawPayloadsController } from "./RawPayloadsController";
 import { RawPayloadsService } from "./RawPayloadsService";
-import { RoleRawPayloadsPlatformAdmin } from "./roles/RoleRawPayloadsPlatformAdmin";
 
 export class RawPayloadsModule extends Module {
   private rawPayloadsService: RawPayloadsService;
@@ -16,8 +15,5 @@ export class RawPayloadsModule extends Module {
 
     this.plugin.api["device-manager/rawPayloads"] =
       this.rawPayloadsController.definition;
-
-    this.plugin.imports.roles[RoleRawPayloadsPlatformAdmin.name] =
-      RoleRawPayloadsPlatformAdmin.definition;
   }
 }
