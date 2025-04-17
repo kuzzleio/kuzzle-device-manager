7b727fceea2f1c1a58baaeeffb39546f6c88c46b
Cyril Nguyen <cnguyen@kuzzle.io>
2025-04-17 17:46:10 +0200

    fix: asset commons case

--- lib/modules/model/ModelsController.ts
+++ lib/modules/model/ModelsController.ts
@@ -137,7 +137,7 @@ export class ModelsController {
 
   async getAsset(request: KuzzleRequest): Promise<ApiModelGetAssetResult> {
     const model = request.getString("model");
-    const engineGroup: string | undefined = request.input.args.engineGroup;
+    const engineGroup = request.getString("engineGroup", "commons");
 
     const assetModel = await this.modelService.getAsset(engineGroup, model);
 
