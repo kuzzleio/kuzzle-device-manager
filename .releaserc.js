6d4677e21782b8e9dd097f2ef97326a4fdf1dab0
drichard-k <drichard@kuzzle.io>
2026-03-09 14:32:22 +0100

    fix(ci): fix semantic-release config

--- .releaserc.js
+++ .releaserc.js
@@ -3,7 +3,7 @@ module.exports = {
   branches: [
     { name: 'master' },
     { name: 'beta', prerelease: true },
-    { name: '2-dev', prerelease: 'dev', range: '2.x' },
+    { name: '2-dev', prerelease: 'dev' },
     { name: 'next', prerelease: true },
   ],
 };
